03c262832664a6ac8738b62d90e61e5fe631d482
MrHongHoang <mrh90tn@gmail.com>
2023-03-17 19:09:07 +0700

    tests

--- models/Bills.js
+++ models/Bills.js
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose'
 
 const billSchema = new mongoose.Schema ({
-    billItems: [
-        name: {type: String, require: true}
-        name: {type: String, require: true}
-        name: {type: String, require: true}
-        name: {type: String, require: true}
+    billItems: [{
+
+        name: {type: String, require: true},
+    }
+        
     ]
-})
+});
+
+const Bill = mongoose.model('Bill', billSchema);
+
+module.exports = Bill
