643e81f733b44be52ebcd7b595e83a7d3726160d
MrHongHoang <mrh90tn@gmail.com>
2023-03-17 18:55:04 +0700

    up test

---
+++ models/Bills.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const billSchema = new mongoose.Schema ({
+    billItems: [
+        name: {type: String, require: true}
+        name: {type: String, require: true}
+        name: {type: String, require: true}
+        name: {type: String, require: true}
+    ]
+})
